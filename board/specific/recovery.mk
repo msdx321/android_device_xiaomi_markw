0f2a570bfe9895d40b93a2a1d96d2448e3a37b99
msdx321 <msdx321@gmail.com>
2016-12-28 18:19:47 +0800

    markw: pack firmwares

--- board/specific/recovery.mk
+++ board/specific/recovery.mk
@@ -3,3 +3,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
+TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
