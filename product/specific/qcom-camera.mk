5697d17a33a83af41fc56e7c0187c4f22e165df6
msdx321 <msdx321@gmail.com>
2017-01-01 18:27:43 +0800

    markw: update camera prop

--- product/specific/qcom-camera.mk
+++ product/specific/qcom-camera.mk
@@ -1,7 +1,3 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.tencent.mm,com.google.android.GoogleCamera \
-    media.camera.ts.monotonic=0 \
-    persist.camera.llnoise=1 \
-    persist.camera.llc=1 \
-    persist.camera.imglib.fddsp=1
+    camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.tencent.mm,com.google.android.GoogleCamera
