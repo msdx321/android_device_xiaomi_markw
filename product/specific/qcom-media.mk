d27b572ed5e70e595cf5439cfe317b403666a9f6
msdx321 <msdx321@gmail.com>
2017-01-04 22:45:44 +0800

    Revert "markw: bring back dashplayer"
    This reverts commit cd67c01b1f13df7283947b831383e1ab3945fe0d.

--- product/specific/qcom-media.mk
+++ product/specific/qcom-media.mk
@@ -1,10 +1,3 @@
 # OMX
 PRODUCT_PACKAGES += \
-    libc2dcolorconvert \
-    libqcmediaplayer \
-    libdashplayer \
-    libextmedia_jni \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+    libc2dcolorconvert
