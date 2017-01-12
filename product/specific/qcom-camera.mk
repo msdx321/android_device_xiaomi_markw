3c5329379d5b7e420be7e42c6f6350e6f237fdcc
msdx321 <msdx321@gmail.com>
2017-01-12 13:27:27 +0800

    markw: add persist.camera.is_type=1

--- product/specific/qcom-camera.mk
+++ product/specific/qcom-camera.mk
@@ -1,3 +1,6 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.tencent.mm,com.google.android.GoogleCamera
+    camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.tencent.mm,com.google.android.GoogleCamera \
+    media.camera.ts.monotonic=0 \
+    persist.camera.gyro.android=1 \
+    persist.camera.is_type=1
