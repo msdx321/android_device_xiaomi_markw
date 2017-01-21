ab208628deeaf2a8bc9da1674103652eef8506ac
msdx321 <msdx321@gmail.com>
2017-01-21 17:27:15 +0800

    markw: misc change

--- product/specific/qcom-camera.mk
+++ product/specific/qcom-camera.mk
@@ -1,6 +1,4 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.tencent.mm,com.google.android.GoogleCamera \
-    media.camera.ts.monotonic=0 \
-    persist.camera.gyro.android=1 \
-    persist.camera.is_type=1
+    camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.google.android.GoogleCamera \
+    media.camera.ts.monotonic=0
