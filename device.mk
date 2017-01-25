fa655593f8591724b0346edd4cb5824865f1325c
msdx321 <msdx321@gmail.com>
2017-01-25 19:59:46 +0800

    markw: use prebuilt lights HAL

--- device.mk
+++ device.mk
@@ -30,7 +30,6 @@ include $(COMMON_PATH)/common/fingerprint.mk
 include $(COMMON_PATH)/common/fm.mk
 include $(COMMON_PATH)/common/gello.mk
 include $(COMMON_PATH)/common/gps.mk
-include $(COMMON_PATH)/common/lights.mk
 include $(COMMON_PATH)/common/media.mk
 include $(COMMON_PATH)/common/snap.mk
 include $(COMMON_PATH)/common/wifi.mk
